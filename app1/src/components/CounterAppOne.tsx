de2cfc99db6c6c457bce05ed2befa1c37cd21a46
Trần Quang Huy <huy.com2003@gmail.com>
2024-05-17 09:07:26 +0700

    Commit

--- app1/src/components/CounterAppOne.tsx
+++ app1/src/components/CounterAppOne.tsx
@@ -1,18 +1,65 @@
 import { Text, Button, Flex } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Card, Col, Row } from "antd";
+import Meta from "antd/es/card/Meta";
 
 const Counter = () => {
-	const [count, setCount] = useState(0);
+  type dataLaptop = {
+    id: string;
+    storeID: string;
+    classify: string;
+    name: string;
+    description: string;
+    price: number;
+    quantity: number;
+    isAvailable: boolean;
+    image: [string];
+  };
 
-	return (
-		<Flex color="#000" gap="1rem" direction="column">
-			<Text>
-				Add by one each click <strong>APP-1</strong>
-			</Text>
-			<Text>Your click count : {count} </Text>
-			<Button onClick={() => setCount(count + 1)}>Click me</Button>
-		</Flex>
-	);
+  const [laptops, setLaptops] = useState<dataLaptop[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:7133/laptops")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setLaptops(data);
+      });
+  }, []);
+  return (
+    <>
+      <div></div>
+      <Row gutter={16}>
+        {laptops.map((item) => (
+          <div>
+            <Col span={8}>
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={
+                  <img
+                    alt="example"
+                    src={`data:image/jpeg;base64,${item.image[0]}`}
+                  ></img>
+                }
+                key={item.id}
+                onClick={() => {
+                  console.log(item.id);
+                }}
+              >
+                <Meta
+                  title={item.name}
+                  description={item.price.toLocaleString()}
+                />
+              </Card>
+            </Col>
+          </div>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Counter;
